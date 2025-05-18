c3d3ad3a5ff414549ab77897d8203089c7406777
OHomemLevelUp <pedro.pellegrina.alba@gmail.com>
2025-05-18 12:47:54 -0300

    Avaliação Concluída
    Terminei a avaliação

---
+++ Mensal3/vue_flask_filmes/frontend-vue/frontend-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <LoginPage v-if="!autenticado" @logado="autenticado = true" />
+    <FilmesView v-else />
+  </div>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+import FilmesView from './components/FilmesView.vue'
+
+export default {
+  components: { LoginPage, FilmesView },
+  data() {
+    return {
+      autenticado: false
+    }
+  }
+}
+</script>
